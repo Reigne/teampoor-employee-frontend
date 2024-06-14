603fd8923ff47b5d7a69a818989aa74470d45782
Reigne <monteronaelija@yahoo.com>
2024-06-14 22:06:05 +0800

    fixed and added delete function

--- Screens/Admin/AppointmentAddService.js
+++ Screens/Admin/AppointmentAddService.js
@@ -14,11 +14,10 @@ import {
 } from "react-native";
 
 import { Divider, ScrollView } from "native-base";
-import { useFocusEffect } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { Searchbar } from "react-native-paper";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from "@react-navigation/native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -41,25 +40,48 @@ const AppointmentAddService = (props) => {
 
   const [services, setServices] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedService, setSelectedService] = useState("");
+  const [errors, setErrors] = useState({});
+  const [appointment, setAppointment] = useState([]);
+
+  const appointmentId = item._id;
 
   useFocusEffect(
     useCallback(() => {
-      axios.get(`${baseURL}services`).then((res) => {
-        setServices(res.data);
-        setLoading(false);
-      });
+      fetchService();
+      fetchAppointment();
 
       return () => {
         setServices([]);
+        setAppointment([]);
         setLoading(false);
       };
-    }, [item])
+    }, [])
   );
+
+  const fetchService = () => {
+    axios.get(`${baseURL}services`).then((res) => {
+      setServices(res.data);
+      setLoading(false);
+    });
+  };
+
+  const fetchAppointment = () => {
+    axios
+      .get(`${baseURL}appointments/${appointmentId}`)
+      .then((res) => {
+        setAppointment(res.data);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setLoading(false); // Set loading state to false when fetching is completed
+      });
+  };
 
   const data = services.map((service) => ({
     label: `${service.name}`,
-    value: service._id, // Assuming _id is the unique identifier for mechanics
-    image: service.images[0]?.url || "", // Assuming _id is the unique identifier for mechanics
+    value: service._id,
+    image: service.images[0]?.url || "",
   }));
 
   const renderServices = (service) => {
@@ -86,7 +108,105 @@ const AppointmentAddService = (props) => {
     );
   };
 
-  console.log(services, "services");
+  const validateForm = () => {
+    let errors = {};
+
+    if (!selectedService) errors.selectedService = "Please choose service.";
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
+
+  const submitHandler = () => {
+    if (!validateForm()) {
+      return;
+    }
+
+    const data = {
+      id: selectedService,
+    };
+
+    console.log(appointmentId, "appointmentId");
+
+    try {
+      axios
+        .put(`${baseURL}appointments/add-service/${appointmentId}`, data)
+        .then((res) => {
+          fetchAppointment();
+
+          Toast.show({
+            topOffset: 60,
+            type: "success",
+            text1: "Successfully Added",
+            text2: `Added new service`,
+          });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          Toast.show({
+            topOffset: 60,
+            type: "error",
+            text1: "Additional Service Error",
+            text2:
+              error.response?.data ||
+              "Something went wrong. Please try again later.",
+          });
+        });
+    } catch (error) {
+      console.log("Catch Error:", error);
+      Toast.show({
+        topOffset: 60,
+        type: "error",
+        text1: "Additional Payment Error",
+        text2:
+          error.response?.data ||
+          "Something went wrong. Please try again later.",
+      });
+    }
+  };
+
+  const deleteHandler = (id) => {
+    try {
+      axios
+        .delete(`${baseURL}appointments/delete-service/${appointmentId}`, {
+          data: { id },
+        })
+        .then((res) => {
+          fetchAppointment();
+
+          Toast.show({
+            topOffset: 60,
+            type: "success",
+            text1: "Successfully Deleted",
+            text2: `Service Deleted`,
+          });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          Toast.show({
+            topOffset: 60,
+            type: "error",
+            text1: "Delete Service Error",
+            text2:
+              error.response?.data ||
+              "Something went wrong. Please try again later.",
+          });
+        });
+    } catch (error) {
+      console.log("Catch Error:", error);
+      Toast.show({
+        topOffset: 60,
+        type: "error",
+        text1: "Delete Service Error",
+        text2:
+          error.response?.data ||
+          "Something went wrong. Please try again later.",
+      });
+    }
+  };
+
+  console.log(selectedService, "selectedService");
 
   return (
     <KeyboardAwareScrollView>
@@ -94,58 +214,70 @@ const AppointmentAddService = (props) => {
         <View className="bg-white p-3 rounded-xl space-y-1">
           <Text className="text-xl font-bold">Additional Services</Text>
           <View className="flex flex-col gap-2">
-            <View className="bg-zinc-100 rounded-xl p-2 w-full">
-              <Dropdown
-                placeholderStyle={styles.placeholderStyle}
-                selectedTextStyle={styles.selectedTextStyle}
-                inputSearchStyle={styles.inputSearchStyle}
-                iconStyle={styles.iconStyle}
-                data={data}
-                search
-                maxHeight={300}
-                className="w-full"
-                labelField="label"
-                valueField="value"
-                placeholder="Select mechanic"
-                searchPlaceholder="Search..."
-                //   value={item?.mechanic?._id || selectedMechanic}
-                //   onChange={(mechanic) => {
-                //     setSelectedMechanic(mechanic.value);
-                //   }}
-                //   renderLeftIcon={() => (
-                //     <UserIcon style={styles.icon} size={15} color="black" />
-                //   )}
-                renderItem={renderServices}
-              />
+            <View>
+              <View
+                className={
+                  errors.selectedService
+                    ? "border border-red-500 bg-zinc-100 rounded-xl p-2 w-full"
+                    : "bg-zinc-100 rounded-xl p-2 w-full"
+                }
+              >
+                <Dropdown
+                  placeholderStyle={styles.placeholderStyle}
+                  selectedTextStyle={styles.selectedTextStyle}
+                  inputSearchStyle={styles.inputSearchStyle}
+                  iconStyle={styles.iconStyle}
+                  data={data}
+                  search
+                  maxHeight={300}
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Select service"
+                  searchPlaceholder="Search..."
+                  value={selectedService}
+                  onChange={(service) => {
+                    setSelectedService(service.value);
+                  }}
+                  renderItem={renderServices}
+                />
+              </View>
+              {errors.selectedService ? (
+                <Text className="text-sm text-red-500">
+                  {errors.selectedService}
+                </Text>
+              ) : null}
             </View>
 
-            <View className="bg-red-500 p-3 rounded-xl">
+            <TouchableOpacity
+              className="bg-red-500 p-3 rounded-xl"
+              onPress={() => submitHandler()}
+            >
               <Text className="text-white font-semibold text-center">
                 Add Service
               </Text>
-            </View>
+            </TouchableOpacity>
           </View>
         </View>
 
         <View className="bg-white p-3 rounded-xl space-y-2">
           <View className="space-y-1">
-            {item?.appointmentServices?.map((service, index) => (
-              <View className="bg-zinc-100 p-2 rounded flex flex-row space-x-2 items-center">
+            {appointment?.appointmentServices?.map((service, index) => (
+              <View className="bg-zinc-100 p-2 rounded flex flex-row space-x-2 items-start">
                 <Image
                   className="rounded"
                   style={{
-                    width: 44,
-                    height: 44,
+                    width: 80,
+                    height: 80,
                   }}
                   source={
-                    service.service?.images[0]?.url
+                    service?.service?.images[0]?.url
                       ? { uri: service.service?.images[0]?.url }
                       : require("../../assets/images/teampoor-default.png")
                   }
                   alt="images"
                 />
 
-                <View className="flex-1 flex-row justify-between items-center">
+                <View className="flex-1 flex-row justify-between items-start">
                   <View className="w-1/2">
                     <Text className="text-xs font-semibold">
                       {service?.service?.name}
@@ -156,16 +288,25 @@ const AppointmentAddService = (props) => {
                       numberOfLines={2}
                       ellipsizeMode="tail"
                     >
-                      {service.service.description}
+                      {service?.service?.description}
                     </Text>
                   </View>
                   <Text className="text-xs">
                     ₱
-                    {service.service.price?.toLocaleString(undefined, {
+                    {service?.service?.price?.toLocaleString(undefined, {
                       minimumFractionDigits: 2,
                       maximumFractionDigits: 2,
                     })}
                   </Text>
+
+                  {/* <Text>X</Text> */}
+                  <View>
+                    <XMarkIcon
+                      color="red"
+                      size={18}
+                      onPress={() => deleteHandler(service._id)}
+                    />
+                  </View>
                 </View>
               </View>
             ))}
